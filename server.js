ab8d7843103fcfc826d4a0dacd469a37431e4fd9
Jacob Richardson <jrichardson@paviliongift.com>
2019-04-03 15:07:02 -0400

    WIP: Updating the spelling of some of the options.
    This commit is updating the spelling and regex of the options. This commit also adds some test files. A couple of things still need to be worked out,
    but it seems to be server video correctly.

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-	, join = require('path').join
-	, basename = require('path').basename
-	, resolve = require('path').resolve
-	, argv = require('optimist').argv
-	, useyHttp = require('usey-http')
-	, server = useyHttp()
-	, glob = require('glob')
-	, rs = require('render-sender')
-	, debug = require('debug')('image-server')
-	, port = process.env.PORT || argv.port || 5556
-	, root = process.env.IMAGE_SERVE_ROOT || argv.root
-	, cache = process.env.IMAGE_SERVE_CACHE || argv.cache
-	, mount = process.env.IMAGE_SERVE_MOUNT || argv.mount || '/image'
-	, maxWidth = process.env.IMAGE_SERVE_MAX_WIDTH || argv.maxWidth || 3000
-	, maxHeight = process.env.IMAGE_SERVE_MAX_HEIGHT || argv.maxHeight || 3000
-	, maxAge = process.env.IMAGE_SERVE_MAX_AGE || argv.maxAge
-	, trim = process.env.IMAGE_SERVE_TRIM || argv.trim
-	, minify = process.env.IMAGE_SERVE_MINIFY || argv.minify
-	, aspectRatio = process.env.IMG_SERVER_ASPECT_RATIO || argv.aspectRatio
-	, bitRate = process.env.BIT_RATE || argv.bitRate
-	, frameRate = process.env.FRAME_RATE || argv.frameRate
-	, timeStamp = process.env.TIMESTAMP || argv.timestamp
-	;
+var fs = require('fs'),
+	join = require('path').join,
+	basename = require('path').basename,
+	resolve = require('path').resolve,
+	argv = require('optimist').argv,
+	useyHttp = require('usey-http'),
+	server = useyHttp(),
+	glob = require('glob'),
+	rs = require('render-sender'),
+	debug = require('debug')('image-server'),
+	port = process.env.PORT || argv.port || 5556,
+	root = process.env.IMAGE_SERVE_ROOT || argv.root,
+	cache = process.env.IMAGE_SERVE_CACHE || argv.cache,
+	mount = process.env.IMAGE_SERVE_MOUNT || argv.mount || '/image',
+	maxWidth = process.env.IMAGE_SERVE_MAX_WIDTH || argv.maxWidth || 3000,
+	maxHeight = process.env.IMAGE_SERVE_MAX_HEIGHT || argv.maxHeight || 3000,
+	maxAge = process.env.IMAGE_SERVE_MAX_AGE || argv.maxAge,
+	trim = process.env.IMAGE_SERVE_TRIM || argv.trim,
+	minify = process.env.IMAGE_SERVE_MINIFY || argv.minify,
+	aspect_ratio = process.env.IMG_SERVER_ASPECT_RATIO || argv.aspect_ratio,
+	bitrate = process.env.BIT_RATE || argv.bitrate,
+	framerate = process.env.FRAME_RATE || argv.framerate,
+	timestamp = process.env.TIMESTAMP || argv.timestamp;
 
 if (!root) {
 	console.error('--root is required to run');
@@ -39,8 +38,8 @@ root = resolve(root);
 cache = resolve(cache);
 
 var irs = rs({
-	maxAge : maxAge
-	, cache : cache
+	maxAge: maxAge,
+	cache: cache
 });
 
 server.use(function (req, res, next) {
@@ -67,12 +66,16 @@ server.get(mount + '/:name/list.json', function (req, res, next) {
 server.get(mount + '/:name', renderImage);
 
 //a terrible default 404 handler
-server.use(useyHttp._404({ message : 'Image Not Found' }));
+server.use(useyHttp._404({
+	message: 'Image Not Found'
+}));
 
 //dump the stack and end when an error is caught
 server.use('error', function (err, req, res, next) {
 	if (err.code === 'ENOENT') {
-		useyHttp._404({ message : 'Image Not Found' })(req, res);
+		useyHttp._404({
+			message: 'Image Not Found'
+		})(req, res);
 	};
 
 	res.end(err.stack);
@@ -85,7 +88,7 @@ server.listen(port);
 console.log('listening on port %s', port);
 
 //This function will serve a cached image or render the new image, save it and send it
-function renderImage (req, res, next) {
+function renderImage(req, res, next) {
 	var opts = new ParsedName(req.params.name);
 
 	opts.path = join(root, opts.name);
@@ -99,14 +102,17 @@ function renderImage (req, res, next) {
 
 	//Video opts.
 
-	opts.aspectRatio = (opts.aspectRatio != null) ? opts.aspectRatio : aspectRatio;
-	opts.bitRate = (opts.bitRate != null) ? opts.bitRate : bitRate;
-	opts.frameRate = (opts.frameRate != null) ? opts.frameRate : frameRate;
+	opts.aspect_ratio = (opts.aspect_ratio != null) ? opts.aspect_ratio : aspect_ratio;
+	opts.bitrate = (opts.bitrate != null) ? opts.bitrate : bitrate;
+	opts.framerate = (opts.framerate != null) ? opts.framerate : framerate;
+	opts.timestamp = (opts.timestamp != null) ? opts.timestamp : timestamp;
 
 	debug(opts);
 
 	opts.req = req;
 	opts.res = res;
+
+	console.log("\n" + opts.path + "\n");
 
 	irs(opts, function (err) {
 		if (err) {
@@ -117,21 +123,21 @@ function renderImage (req, res, next) {
 };
 
 ParsedName.OPTIONS = {
-	dimensions :  /-(([0-9]+)x([0-9]+))/
-	, crop : /-(cropped|crop):([0-9]+)x([0-9]+)~([0-9]+),([0-9]+)/
-	, trim : /-trimmed|-trim/
-	, notrim : /-notrim|-nottrimmed/
-	, minify : /-minify|-minified/
-	, nominify : /-nominify|-notminified/
-	, format : /\.([a-zA-Z]+)$/
-	, sourceFormat : /\.([a-zA-Z]+)\.[a-zA-Z]+$/
-	, aspectRatio: /-aspectRatio|-aspectRatio/
-	, bitRate : /-bitRate|-bitRate/
-	, frameRate : /-bitRate|-bitRate/
-	, timestamp : /-timestamp|-timestamp/
+	dimensions: /-(([0-9]+)x([0-9]+))/,
+	crop: /-(cropped|crop):([0-9]+)x([0-9]+)~([0-9]+),([0-9]+)/,
+	trim: /-trimmed|-trim/,
+	notrim: /-notrim|-nottrimmed/,
+	minify: /-minify|-minified/,
+	nominify: /-nominify|-notminified/,
+	format: /\.([a-zA-Z0-9]+)$/,
+	sourceFormat: /\.([a-zA-Z0-9]+)\.[a-zA-Z0-9]+$/,
+	bitrate: /-bitrate:([0-9]{1,4})/,
+	framerate: /-framerate:([0-9]{1,3})/,
+	timestamp: /-timestamp:([0-9\.]{1,9})/,
+	apsect_ratio: /-aspect_ratio:([([0-9:])])/
 };
 
-function ParsedName (name) {
+function ParsedName(name) {
 	var fileName = [];
 	var foundOptions = false;
 
@@ -157,8 +163,7 @@ function ParsedName (name) {
 
 		if (i == a.length - 1) {
 			fileName.push(token.split('.')[0]);
-		}
-		else {
+		} else {
 			fileName.push(token);
 		}
 	});
@@ -168,33 +173,35 @@ function ParsedName (name) {
 
 	this.sourceFormat = (ParsedName.OPTIONS.sourceFormat.exec(name) || "")[1] || null;
 	this.format = (ParsedName.OPTIONS.format.exec(name) || "")[1] || null;
-	this.name = fileName.join('-') + '.' + (this.sourceFormat || this.format);
+	this.sourceFormat = this.sourceFormat || this.format;
 
-	this.aspectRatio = (ParsedName.OPTIONS.aspectRatio.exec(name) || "")[1] || null;
-	this.bitRate = (ParsedName.OPTIONS.bitRate.exec(name) || "")[1] || null;
-	this.frameRate = (ParsedName.OPTIONS.frameRate.exec(name) || "")[1] || null;
+	console.log(this.sourceFormat);
+	console.log(this.format);
+
+
+	this.name = fileName.join('-') + '.' + (this.sourceFormat || this.format || 'mp4');
+
+	this.bitrate = (ParsedName.OPTIONS.bitrate.exec(name) || "")[1] || null;
+	this.framerate = (ParsedName.OPTIONS.framerate.exec(name) || "")[1] || null;
 	this.timestamp = (ParsedName.OPTIONS.timestamp.exec(name) || "")[1] || null;
 
-	this.trim = ParsedName.OPTIONS.trim.test(name)
-		? true
-		: ParsedName.OPTIONS.notrim.test(name)
-		? false
-		: null
-		;
+	this.trim = ParsedName.OPTIONS.trim.test(name) ?
+		true :
+		ParsedName.OPTIONS.notrim.test(name) ?
+		false :
+		null;
 
-	this.minify = ParsedName.OPTIONS.minify.test(name)
-		? true
-		: ParsedName.OPTIONS.nominify.test(name)
-		? false
-		: null
-		;
+	this.minify = ParsedName.OPTIONS.minify.test(name) ?
+		true :
+		ParsedName.OPTIONS.nominify.test(name) ?
+		false :
+		null;
 
 	if (dimensions) {
 		this.dimensions = dimensions[1];
 		this.width = dimensions[2];
 		this.height = dimensions[3];
-	}
-	else {
+	} else {
 		this.dimensions = null;
 		this.width = null;
 		this.height = null;
@@ -202,13 +209,12 @@ function ParsedName (name) {
 
 	if (crop) {
 		this.crop = {
-			width : crop[2]
-			, height : crop[3]
-			, x : crop[4]
-			, y : crop[5]
+			width: crop[2],
+			height: crop[3],
+			x: crop[4],
+			y: crop[5]
 		};
-	}
-	else {
+	} else {
 		this.crop = null;
 	}
 }
